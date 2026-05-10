dd5b586049d58938d4dbc5c545c3a0cd98e759b6
Akmal <akmalthemalong@gmail.com>
2026-05-10 22:12:13 +0800

    hapus tombol

--- resources/views/livewire/table/alternatif-table.blade.php
+++ resources/views/livewire/table/alternatif-table.blade.php
@@ -16,16 +16,6 @@ use App\Enums\State;
                                 <h5 class="modal-title text-white" id="myModalLabel1">
                                     Data Alternatif Siswa
                                 </h5>
-                                <button type="button" class="close rounded-pill"
-                                    wire:click="$dispatch('closeModal', {id: 'modal-form-siswa'})">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                        class="feather feather-x">
-                                        <line x1="18" y1="6" x2="6" y2="18"></line>
-                                        <line x1="6" y1="6" x2="18" y2="18"></line>
-                                    </svg>
-                                </button>
                             </div>
                             <div class="modal-body">
 <!-- FIX: modal tidak bisa tertutup -->
